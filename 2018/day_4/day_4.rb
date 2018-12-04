38988190fb7feb0dae45460ccbe1731274e6687d
zeddan <robin.saaf@gmail.com>
2018-12-04 19:29:09 +0100

    Solutions for day 4 part 2, 2018
    Also some minor refactoring

--- 2018/day_4/day_4.rb
+++ 2018/day_4/day_4.rb
@@ -1,6 +1,22 @@
 class Challenge
 
   def solve_part_1(input)
+    id, sleeps = sleeps(input).max_by{ |k,v| v[:total] }
+    times = sleeps[:times].flatten
+    id.to_i * times.max_by{ |t| times.count(t) }
+  end
+
+  def solve_part_2(input)
+    sleeps = sleeps(input).map do |s|
+      { id: s.first,
+        longest: s.last[:times].flatten.group_by(&:itself).values
+                  .sort{ |a,b| a.length <=> b.length }.last }
+    end
+    sleepiest_guard = sleeps.max_by { |s| s[:longest].length }
+    sleepiest_guard[:id].to_i * sleepiest_guard[:longest].first
+  end
+
+  def sleeps(input)
     input.sort! { |a, b| a[0...18] <=> b[0...18] }
     guard, sleep = "", 0
     sleeps = Hash.new{ |h,k| h[k] = { total: 0, times: []  } }
@@ -13,12 +29,10 @@ class Challenge
       when "w" then
         wokeup = i[15..16].to_i
         sleeps[guard][:total] += wokeup - sleep
-        sleeps[guard][:times] << (sleep..wokeup)
+        sleeps[guard][:times] << (sleep..wokeup).to_a
       end
     }
-    id, sleeps = sleeps.max_by{ |k,v| v[:total] }
-    times = sleeps[:times].map(&:to_a).flatten
-    id.to_i * times.max_by{ |t| times.count(t) }
+    sleeps
   end
 
 end
