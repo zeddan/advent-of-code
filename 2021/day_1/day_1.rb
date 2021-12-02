324fe4f399344940f67fd9a0cc165437e2cc489d
zeddan <robin.saaf@gmail.com>
2021-12-02 22:01:11 +0100

    Day 1 part 2
    It's beautiful

--- 2021/day_1/day_1.rb
+++ 2021/day_1/day_1.rb
@@ -18,5 +18,16 @@ class Challenge
   end
 
   def part_2
+    increases = 0
+
+    input[0...-3].each.with_index(4) do |line, i|
+      window_1 = input[i-2] + input[i-3] + input[i-4]
+      window_2 = input[i-1] + input[i-2] + input[i-3]
+      if window_2 > window_1
+        increases += 1
+      end
+    end
+
+    increases
   end
 end
