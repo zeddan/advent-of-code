bf749e75294f2ac3924a938bea725fe7ff05f524
zeddan <robin.saaf@gmail.com>
2022-11-20 22:54:56 +0100

    Day 6 part 2

--- 2021/day_6/day_6.rb
+++ 2021/day_6/day_6.rb
@@ -6,23 +6,28 @@ class Challenge
   end
 
   def part_1
-    80.times do |t|
-      added = 0
-      new_input = input.map do |x|
-        if x > 0
-          x - 1
-        elsif x == 0
-          added += 1
-          6
-        end
-      end
-      added.times { new_input << 8 }
-      @input = new_input
-    end
-
-    input.size
+    solve(80)
   end
 
   def part_2
+    solve(256)
+  end
+
+  def solve(days)
+    state = {0=>0,1=>0,2=>0,3=>0,4=>0,5=>0,6=>0,7=>0,8=>0}
+    input.group_by(&:itself).transform_values { |v| v.size }.each { |k,v| state[k] = v }
+    days.times do |t|
+      tmp = state.dup
+      state[0] = tmp[1]
+      state[1] = tmp[2]
+      state[2] = tmp[3]
+      state[3] = tmp[4]
+      state[4] = tmp[5]
+      state[5] = tmp[6]
+      state[6] = tmp[0] + tmp[7]
+      state[7] = tmp[8]
+      state[8] = tmp[0]
+    end
+    state.map { |k,v| v }.sum
   end
 end
