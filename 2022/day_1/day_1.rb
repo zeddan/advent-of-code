6c7c511d5f70f02de7aa7e9a9b75303035c6d54c
zeddan <robin.saaf@gmail.com>
2022-12-01 22:28:45 +0100

    2022 day 1 part 2

--- 2022/day_1/day_1.rb
+++ 2022/day_1/day_1.rb
@@ -6,20 +6,28 @@ class Challenge
   end
 
   def part_1
+    find_max
+  end
+
+  def part_2
+    max1 = find_max
+    max2 = find_max(max1)
+    max3 = find_max(max2)
+    max1+max2+max3
+  end
+
+  def find_max(current=100000)
     max = 0
     count = 0
     input.each do |num|
       count += num.to_i
       if num == ""
-        if count > max
+        if count > max && count < current
           max = count
         end
         count = 0
       end
     end
     max
-  end
-
-  def part_2
   end
 end
