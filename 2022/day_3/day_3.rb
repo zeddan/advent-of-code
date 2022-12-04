0c556b55d8a218d504919591c590b668f0ba68d6
zeddan <robin.saaf@gmail.com>
2022-12-04 21:08:41 +0100

    2022 day 3 part 2

--- 2022/day_3/day_3.rb
+++ 2022/day_3/day_3.rb
@@ -14,5 +14,15 @@ class Challenge
   end
 
   def part_2
+    idx = 0
+    sum = 0
+    while idx < input.size/3
+      s = input[idx*3...3+idx*3]
+      common = s[0].chars & s[1].chars & s[2].chars
+      letter = common[0]
+      sum += letter == letter.upcase ? letter.ord-38 : letter.ord-96
+      idx += 1
+    end
+    sum
   end
 end
