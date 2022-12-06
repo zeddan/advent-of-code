d3eafda94aa320a0b40599efe6ae28ed632fd1da
zeddan <robin.saaf@gmail.com>
2022-12-06 15:46:23 +0100

    2022 day 6 part 2

--- 2022/day_6/day_6.rb
+++ 2022/day_6/day_6.rb
@@ -6,18 +6,23 @@ class Challenge
   end
 
   def part_1
+    scan(4)
+  end
+
+  def part_2
+    scan(14)
+  end
+
+  def scan(m_size)
     idx = 0
     searching = true
     while searching
-      if input[idx...idx+4].chars.uniq.size == 4
+      if input[idx...idx+m_size].chars.uniq.size == m_size
         searching = false
       else
         idx += 1
       end
     end
-    idx+4
-  end
-
-  def part_2
+    idx+m_size
   end
 end
